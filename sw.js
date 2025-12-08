86fef6c7cf50e93a3f018b4e871b12e4359c8665
pawel9908 <116102073+pawel9908@users.noreply.github.com>
2025-12-08 21:57:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 const ROOT = "/grafik-4brygadowy/";
 
 // Zmieniaj przy każdym deployu
-const CACHE_VERSION = "v20";
+const CACHE_VERSION = "v21";
 const CACHE_NAME = `grafik-cache-${CACHE_VERSION}`;
 
 const ASSETS = [
